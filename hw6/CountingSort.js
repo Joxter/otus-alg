05f2e98f824466eb6c6071cb16abe38c757facdc
joxter <joxterrr@gmail.com>
2021-03-09 16:29:24 +0300

    hw6. improve radixSort and countingSort

--- hw6/CountingSort.js
+++ hw6/CountingSort.js
@@ -3,18 +3,14 @@ import { testRunner } from '../test-runner.js';
 /**
  * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
- * Test #2   [OK]  time: 0:00.001
- * Test #3   [OK]  time: 0:00.001
- * Test #4   [OK]  time: 0:00.012
- * Test #5   [OK]  time: 0:00.058
- * Test #6   [OK]  time: 0:00.654
- * Test #7   [OK]  time: 0:07.388
+ * Test #2   [OK]  time: 0:00.000
+ * Test #3   [OK]  time: 0:00.000
+ * Test #4   [OK]  time: 0:00.007
+ * Test #5   [OK]  time: 0:00.040
+ * Test #6   [OK]  time: 0:00.453
+ * Test #7   [OK]  time: 0:05.293
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\0.random\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = countingSort(arrStr.split(' ').map(n => +n), k);
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\0.random\\', testCountingSort);
 
 /**
  * Test #0   [OK]  time: 0:00.000
@@ -22,58 +18,53 @@ testRunner('C:\\projects\\otus-alg\\hw5\\0.random\\', (str) => {
  * Test #2   [OK]  time: 0:00.000
  * Test #3   [OK]  time: 0:00.000
  * Test #4   [OK]  time: 0:00.001
- * Test #5   [OK]  time: 0:00.015
- * Test #6   [OK]  time: 0:00.150
- * Test #7   [OK]  time: 0:01.615
+ * Test #5   [OK]  time: 0:00.008
+ * Test #6   [OK]  time: 0:00.095
+ * Test #7   [OK]  time: 0:01.087
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\1.digits\\', (str) => {
-  const [, arrStr] = str.trim().split('\n');
-  const result = countingSort(arrStr.split(' ').map(n => +n), 10);
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\1.digits\\', testCountingSort);
 
 /**
  * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
  * Test #2   [OK]  time: 0:00.000
  * Test #3   [OK]  time: 0:00.003
- * Test #4   [OK]  time: 0:00.005
- * Test #5   [OK]  time: 0:00.068
- * Test #6   [OK]  time: 0:00.581
- * Test #7   [OK]  time: 0:06.196
+ * Test #4   [OK]  time: 0:00.004
+ * Test #5   [OK]  time: 0:00.040
+ * Test #6   [OK]  time: 0:00.375
+ * Test #7   [OK]  time: 0:04.082
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\2.sorted\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = countingSort(arrStr.split(' ').map(n => +n), k);
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\2.sorted\\', testCountingSort);
 
 /**
  * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
  * Test #2   [OK]  time: 0:00.000
  * Test #3   [OK]  time: 0:00.000
- * Test #4   [OK]  time: 0:00.003
- * Test #5   [OK]  time: 0:00.041
- * Test #6   [OK]  time: 0:00.593
- * Test #7   [OK]  time: 0:06.153
+ * Test #4   [OK]  time: 0:00.002
+ * Test #5   [OK]  time: 0:00.023
+ * Test #6   [OK]  time: 0:00.372
+ * Test #7   [OK]  time: 0:04.044
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\3.revers\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = countingSort(arrStr.split(' ').map(n => +n), k);
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\3.revers\\', testCountingSort);
 
 
-export function countingSort(arr, k) {
-  const counters = Array.from({length: k}).fill(0);
-  const result = Array.from({length: arr.length});
+export function countingSort(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+
+  const counters = Array(max + 1).fill(0);
+  const result = Array(arr.length);
 
   for (let i = 0; i < arr.length; i++) {
     counters[arr[i]]++;
   }
 
-  for (let i = 1; i < k; i++) {
+  for (let i = 1; i < counters.length; i++) {
     counters[i] = counters[i] + counters[i - 1];
   }
 
@@ -83,4 +74,10 @@ export function countingSort(arr, k) {
   }
 
   return result;
+}
+
+function testCountingSort(str) {
+  const [, arrStr] = str.trim().split('\n');
+  const result = countingSort(arrStr.split(' ').map(n => +n));
+  return result.join(' ');
 }
--- hw6/RadixSort.js
+++ hw6/RadixSort.js
@@ -4,77 +4,68 @@ import { testRunner } from '../test-runner.js';
  * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
  * Test #2   [OK]  time: 0:00.000
- * Test #3   [OK]  time: 0:00.004
- * Test #4   [OK]  time: 0:00.017
- * Test #5   [OK]  time: 0:00.156
- * Test #6   [OK]  time: 0:01.902
- * Test #7   [OK]  time: 0:23.247
+ * Test #3   [OK]  time: 0:00.001
+ * Test #4   [OK]  time: 0:00.012
+ * Test #5   [OK]  time: 0:00.048
+ * Test #6   [OK]  time: 0:00.492
+ * Test #7   [OK]  time: 0:05.215
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\0.random\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = radixSort(arrStr.split(' ').map(n => +n), Math.log10(+k));
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\0.random\\', testRadixSort);
 
 /**
- * Test #0   [OK]  time: 0:00.002
+ * Test #0   [OK]  time: 0:00.000
+ * Test #1   [OK]  time: 0:00.000
+ * Test #2   [OK]  time: 0:00.000
+ * Test #3   [OK]  time: 0:00.000
+ * Test #4   [OK]  time: 0:00.001
+ * Test #5   [OK]  time: 0:00.012
+ * Test #6   [OK]  time: 0:00.114
+ * Test #7   [OK]  time: 0:01.221
+ */
+testRunner('C:\\projects\\otus-alg\\hw5\\1.digits\\', testRadixSort);
+
+/**
+ * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
  * Test #2   [OK]  time: 0:00.000
  * Test #3   [OK]  time: 0:00.003
- * Test #4   [OK]  time: 0:00.004
- * Test #5   [OK]  time: 0:00.034
- * Test #6   [OK]  time: 0:00.205
- * Test #7   [OK]  time: 0:01.940
+ * Test #4   [OK]  time: 0:00.006
+ * Test #5   [OK]  time: 0:00.043
+ * Test #6   [OK]  time: 0:00.469
+ * Test #7   [OK]  time: 0:05.324
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\1.digits\\', (str) => {
-  const [, arrStr] = str.trim().split('\n');
-  const result = radixSort(arrStr.split(' ').map(n => +n), 1);
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\2.sorted\\', testRadixSort);
 
 /**
  * Test #0   [OK]  time: 0:00.000
  * Test #1   [OK]  time: 0:00.000
  * Test #2   [OK]  time: 0:00.000
- * Test #3   [OK]  time: 0:00.002
- * Test #4   [OK]  time: 0:00.017
- * Test #5   [OK]  time: 0:00.138
- * Test #6   [OK]  time: 0:01.920
- * Test #7   [OK]  time: 0:23.376
+ * Test #3   [OK]  time : 0:00.000
+ * Test #4   [OK]  time: 0:00.002
+ * Test #5   [OK]  time: 0:00.029
+ * Test #6   [OK]  time: 0:00.466
+ * Test #7   [OK]  time: 0:05.317
  */
-testRunner('C:\\projects\\otus-alg\\hw5\\2.sorted\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = radixSort(arrStr.split(' ').map(n => +n), Math.log10(+k));
-  return result.join(' ');
-});
+testRunner('C:\\projects\\otus-alg\\hw5\\3.revers\\', testRadixSort);
 
-/**
- * Test #0   [OK]  time: 0:00.000
- * Test #1   [OK]  time: 0:00.000
- * Test #2   [OK]  time: 0:00.000
- * Test #3   [OK]  time: 0:00.004
- * Test #4   [OK]  time: 0:00.013
- * Test #5   [OK]  time: 0:00.160
- * Test #6   [OK]  time: 0:01.873
- * Test #7   [OK]  time: 0:23.135
- */
-testRunner('C:\\projects\\otus-alg\\hw5\\3.revers\\', (str) => {
-  const [k, arrStr] = str.trim().split('\n');
-  const result = radixSort(arrStr.split(' ').map(n => +n), Math.log10(+k));
-  return result.join(' ');
-});
+export function radixSort(arr) {
+  let currentRadix = 10;
+  let tempArr = Array(arr.length);
+  const counters = Array(10);
 
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  max = max + 1;
 
-export function radixSort(arr, r) {
-  let currentRadix = 1;
-  let result = Array.from({length: arr.length});
-  const counters = Array.from({length: 10});
-
-  while (currentRadix <= r) {
+  while (currentRadix <= max) {
     counters.fill(0);
 
     for (let i = 0; i < arr.length; i++) {
-      counters[getRadix(arr[i], currentRadix)]++;
+      counters[getDigitByRadix(arr[i], currentRadix)]++;
     }
 
     for (let i = 1; i < counters.length; i++) {
@@ -82,18 +73,26 @@ export function radixSort(arr, r) {
     }
 
     for (let i = arr.length - 1; i >= 0; i--) {
-      const r = getRadix(arr[i], currentRadix);
+      const r = getDigitByRadix(arr[i], currentRadix);
       counters[r]--;
-      result[counters[r]] = arr[i];
+      tempArr[counters[r]] = arr[i];
     }
 
-    arr = [...result];
-    currentRadix++;
+    for (let i = 0; i < tempArr.length; i++) {
+      arr[i] = tempArr[i];
+    }
+    currentRadix *= 10;
   }
 
   return arr;
 }
 
-function getRadix(n, k) {
-  return Math.floor((n % (10 ** k)) / (10 ** (k - 1)));
+function getDigitByRadix(n, k) {
+  return Math.floor((n % k) / (k / 10));
+}
+
+function testRadixSort(str) {
+  const [, arrStr] = str.trim().split('\n');
+  const result = radixSort(arrStr.split(' ').map(n => +n));
+  return result.join(' ');
 }
